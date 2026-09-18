6ce4f9b1a729d657ee035d834ee4e44386590df4
git-retard <irkarharsharaj@gmail.com>
2026-09-18 23:41:59 +0530

    Prebuilt Apps for AxionOS Core

--- Android.mk
+++ Android.mk
@@ -1,12 +1,3 @@
 # Axion Vanilla specific Packages
 PRODUCT_PACKAGES += \
-	QuikSMS \
-	Aurora \
-	Thunderbird \
-	Fdroid \
-	Fpriv \
-	Obsidian \
-	OrganicMap
-#
-PRODUCT_COPY_FILES += \
-vendor/lineage/prebuilt/common/app/F-priv/permissions_org.fdroid.fdroid.privileged.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_org.fdroid.fdroid.privileged.xml
+	QuikSMS
